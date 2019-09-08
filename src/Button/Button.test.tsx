7124621752f88759cacb923381b4eedc5afe5642
Marcelo Dapper <marcelo.dapper@gmail.com>
2019-09-08 02:03:53 -0300

    Add Button tests

---
+++ src/Button/Button.test.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import 'jest-styled-components';
+
+import { Button } from './Button';
+import { Dark, Light } from '../theme';
+
+describe('<Button />', () => {
+  it('renders with Light theme', () => {
+    const { container } = render(
+      <ThemeProvider theme={Dark}>
+        <Button color="primary">Test</Button>
+      </ThemeProvider>
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule('color', '#fff');
+  });
+
+  it('renders with Light theme', () => {
+    const { container } = render(
+      <ThemeProvider theme={Light}>
+        <Button color="primary">Test</Button>
+      </ThemeProvider>
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule('color', '#fff');
+  });
+});
