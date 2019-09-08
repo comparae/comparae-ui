fa40a25abbe1030985215a9c3dd3b0ab7dd3cbd0
Marcelo Dapper <marcelo.dapper@gmail.com>
2019-09-08 12:23:32 -0300

    Add renderWithTheme test util

---
+++ src/utils/tests.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+
+import { Dark, Light } from '../theme';
+
+const themes: any = {
+  dark: Dark,
+  light: Light,
+};
+
+// this is a handy function that I would utilize for any component
+// that relies on the router being in context
+export function renderWithTheme(ui: any, theme: 'dark' | 'light') {
+  return {
+    ...render(<ThemeProvider theme={themes[theme]}>{ui}</ThemeProvider>),
+  };
+}
