775dd5133b51bcd85d777bd41d3ac1fdc9a7896b
Marcelo Dapper <marcelo.dapper@gmail.com>
2019-09-08 12:25:47 -0300

    Fix test to use renderWithTheme util

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -1,27 +1,23 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
-import { ThemeProvider } from 'styled-components';
+import { renderWithTheme } from '../utils/tests';
 
 import { Button } from './Button';
-import { Dark, Light } from '../theme';
 
 describe('<Button />', () => {
   it('renders with Light theme', () => {
-    const { container } = render(
-      <ThemeProvider theme={Dark}>
-        <Button color="primary">Test</Button>
-      </ThemeProvider>
+    const { container } = renderWithTheme(
+      <Button color="primary">Test</Button>,
+      'dark'
     );
 
     expect(container.firstChild).toMatchSnapshot();
-    expect(container.firstChild).toHaveStyleRule('color', '#000');
+    expect(container.firstChild).toHaveStyleRule('color', '#fff');
   });
 
   it('renders with Light theme', () => {
-    const { container } = render(
-      <ThemeProvider theme={Light}>
-        <Button color="primary">Test</Button>
-      </ThemeProvider>
+    const { container } = renderWithTheme(
+      <Button color="primary">Test</Button>,
+      'light'
     );
 
     expect(container.firstChild).toMatchSnapshot();
